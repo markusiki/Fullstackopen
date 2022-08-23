7a620cc797b72731b28dbc865042392624f3ef89
Markus Kitunen <markus.kitunen@outlook.com>
2022-08-23 14:48:02 +0300

    update

--- osa2/frontendharjoitus/src/services/notes.js
+++ osa2/frontendharjoitus/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'https://peaceful-brushlands-05029.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
