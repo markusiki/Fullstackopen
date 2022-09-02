7ffd36dd9622f8d6e465791d58fce03955ff79bf
Markus Kitunen <markus.kitunen@outlook.com>
2022-09-02 21:04:08 +0300

    notes-backend update

--- osa3/notes-backend/index.js
+++ osa3/notes-backend/index.js
@@ -1,31 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
-app.use(cors())
-app.use(express.static('build'))
-
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-01-10T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-01-10T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-01-10T19:20:14.298Z",
-      important: true
-    }
-]
+app.use(express.json())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -35,73 +14,98 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
-
 app.use(requestLogger)
 
+app.use(cors())
+app.use(express.static('build'))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/', (request, response, next) => {
+    response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    res.json(note)
-  }
-
-  else {
-    res.status(404).end()
-  }
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(note => note.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (req, res) => {
-   const body = req.body
-
-   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+      console.log(notes)
+      response.status(401).json(notes)
     })
-   }
+    .catch(error => {
+      console.log(error.message)
+      return response.status(401).send({ error: 'test' })
+    })
+})
 
-   const note = {
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+   const body = request.body
+
+   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-   }
+   })
 
-  notes = notes.concat(note)
-
-  res.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
 })
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
   
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
