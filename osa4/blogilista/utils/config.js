ce5ecf0323510d65f945e927c57eaf4f9a13847c
Markus Kitunen <markus.kitunen@outlook.com>
2022-09-05 18:44:21 +0300

    aplication divided into modules

---
+++ osa4/blogilista/utils/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+let PORT = 3003
+let MONGODB_URI = 'mongodb+srv://MarkusIK:9autwXy1U43Y7o6f@cluster0.ngfhw.mongodb.net/blogilista?retryWrites=true&w=majority'
+
+module.exports = {
+  PORT,
+  MONGODB_URI
+}
