e70a77db4c1dcfd7ba577be079949763b981804d
Markus Kitunen <markus.kitunen@outlook.com>
2022-09-05 20:34:18 +0300

    updated

--- osa3/notes-backend/models/note.js
+++ osa3/notes-backend/models/note.js
@@ -1,30 +1,14 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-.then(result => {
-    console.log('connected to mongoDB')
-})
-.catch((error) => {
-    console.log('error connecting to mongoDB:', error.message)
-})
-
 const noteSchema = new mongoose.Schema({
-    content:
+  content:
     {
       type: String,
       minlength: 5,
       required: true
     },
-    date: 
-    {
-      type: Date,
-      required: true
-    },
-    important: Boolean,
+  date: Date,
+  important: Boolean
 })
 
 noteSchema.set('toJSON', {
